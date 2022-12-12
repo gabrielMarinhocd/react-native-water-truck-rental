adba73dc6a10403709f84bfa6505db541b35ed91
@gabrielmarinhodossantoscd <gabrielmarinhodossantoscd@gmail.com>
2022-12-11 21:27:24 -0300

    Adicionando nova URL backend

--- api/ApiService.js
+++ api/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gabriel-marinho-pipa.herokuapp.com/",
+  baseURL: "https://water-truck.onrender.com/",
 });
 
 export default api;
